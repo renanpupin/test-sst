4a1c50191fea8b381cc4c54f9ebe240f0dae17f1
Renan Pupin <renan.pupin@gmail.com>
2023-10-26 00:37:35 -0300

    ops: fix lambdas

--- packages/functions/src/graphql.ts
+++ packages/functions/src/graphql.ts
@@ -1,34 +1,40 @@
-import { ApolloServer } from '@apollo/server';
-import { startServerAndCreateLambdaHandler, handlers } from '@as-integrations/aws-lambda';
+import {ApolloServer} from '@apollo/server';
+import {startServerAndCreateLambdaHandler, handlers} from '@as-integrations/aws-lambda';
 import {ApiHandler} from "sst/node/api";
 
 const typeDefs = `#graphql
 type Query {
-  hello: String
+    hello: String
 }
 `;
 
 const resolvers = {
-  Query: {
-    hello: () => 'world',
-  },
+    Query: {
+        hello: () => 'world',
+    },
 };
 
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
+    typeDefs,
+    resolvers,
 });
 
 // This final export is important!
 export const gqlHandler = startServerAndCreateLambdaHandler(
     server,
     // We will be using the Proxy V2 handler
-    handlers.createAPIGatewayProxyEventV2RequestHandler(),
-);
-
+    handlers.createAPIGatewayProxyEventV2RequestHandler(), {
+        context: async ({event, context}) => {
+            // console.log('event,context', {event, context})
+            return {
+                lambdaEvent: event,
+                lambdaContext: context,
+            };
+        },
+    });
 export const handler = ApiHandler(async (_evt) => {
-  return {
-    statusCode: 200,
-    body: `Hello from gql api.`,
-  };
+    return {
+        statusCode: 200,
+        body: `Hello from gql api.`,
+    };
 });
