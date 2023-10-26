445b4c8671a1579799c57976fc4125bf6dcf41c1
Renan Pupin <renan.pupin@gmail.com>
2023-10-26 00:39:42 -0300

    ops: cleanup and add prettier

--- packages/functions/src/graphql.ts
+++ packages/functions/src/graphql.ts
@@ -1,40 +1,41 @@
-import {ApolloServer} from '@apollo/server';
-import {startServerAndCreateLambdaHandler, handlers} from '@as-integrations/aws-lambda';
-import {ApiHandler} from "sst/node/api";
+import {ApolloServer} from '@apollo/server'
+import {startServerAndCreateLambdaHandler, handlers} from '@as-integrations/aws-lambda'
+import {ApiHandler} from 'sst/node/api'
 
+//TODO: move to code first with federation support
 const typeDefs = `#graphql
 type Query {
     hello: String
 }
-`;
+`
 
 const resolvers = {
     Query: {
-        hello: () => 'world',
-    },
-};
+        hello: () => 'world'
+    }
+}
 
 const server = new ApolloServer({
     typeDefs,
-    resolvers,
-});
+    resolvers
+})
 
-// This final export is important!
 export const gqlHandler = startServerAndCreateLambdaHandler(
     server,
-    // We will be using the Proxy V2 handler
-    handlers.createAPIGatewayProxyEventV2RequestHandler(), {
+    handlers.createAPIGatewayProxyEventV2RequestHandler(),
+    {
         context: async ({event, context}) => {
             // console.log('event,context', {event, context})
             return {
                 lambdaEvent: event,
-                lambdaContext: context,
-            };
-        },
-    });
-export const handler = ApiHandler(async (_evt) => {
+                lambdaContext: context
+            }
+        }
+    }
+)
+export const handler = ApiHandler(async _evt => {
     return {
         statusCode: 200,
-        body: `Hello from gql api.`,
-    };
-});
+        body: `Hello from gql api.`
+    }
+})
